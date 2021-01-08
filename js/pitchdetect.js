39c3a2f3b3311f1f2386ff8ec474555f2018161e
gsmoon97 <gsmoon97@gmail.com>
2021-01-09 00:37:59 +0800

    Implement game logic

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -21,6 +21,51 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+// define the time limit
+let TIME_LIMIT = 60;
+
+// define quotes to be used
+// let quotes_array = [
+//   "Push yourself, because no one else is going to do it for you.",
+//   "Failure is the condiment that gives success its flavor.",
+//   "Wake up with determination. Go to bed with satisfaction.",
+//   "It's going to be hard, but hard does not mean impossible.",
+//   "Learning never exhausts the mind.",
+//   "The only way to do great work is to love what you do."
+// ];
+
+let quotes_array = [
+  "Push your limit",
+  "Success is on the way",
+  "Wake up bro",
+  "It is showtime",
+  "Learning is so fun",
+  "The man turned around"
+];
+
+// selecting required elements
+let timer_text = document.querySelector(".curr_time");
+let accuracy_text = document.querySelector(".curr_accuracy");
+let error_text = document.querySelector(".curr_errors");
+let cpm_text = document.querySelector(".curr_cpm");
+let wpm_text = document.querySelector(".curr_wpm");
+let quote_text = document.querySelector(".quote");
+let input_area = document.querySelector(".input_area");
+let restart_btn = document.querySelector(".restart_btn");
+let cpm_group = document.querySelector(".cpm");
+let wpm_group = document.querySelector(".wpm");
+let error_group = document.querySelector(".errors");
+let accuracy_group = document.querySelector(".accuracy");
+
+let timeLeft = TIME_LIMIT;
+let timeElapsed = 0;
+let total_errors = 0;
+let errors = 0;
+let accuracy = 0;
+let characterTyped = 0;
+let current_quote = "";
+let quoteNo = 0;
+let game_timer = null;
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
@@ -39,6 +84,8 @@ var timer = 0;
 var randomVowel;
 
 window.onload = function () {
+  console.log("file1");
+
   audioContext = new AudioContext();
   MAX_SIZE = Math.max(4, Math.floor(audioContext.sampleRate / 1000)); // corresponds to a 1kHz signal
 
@@ -73,9 +120,163 @@ window.onload = function () {
         origString.substring(0, origString.length - 22) +
         event.key +
         origString.substring(origString.length - 22);
+        processCurrentText();
     }
   });
 };
+
+function updateQuote() {
+  console.log('calling updateQuote');
+  quote_text.textContent = null;
+  current_quote = quotes_array[quoteNo];
+
+  // separate each character and make an element 
+  // out of each of them to individually style them
+  current_quote.split('').forEach(char => {
+    const charSpan = document.createElement('span')
+    charSpan.innerText = char
+    quote_text.appendChild(charSpan)
+  })
+
+  console.log(quote_text);
+
+  // roll over to the first quote
+  if (quoteNo < quotes_array.length - 1)
+    quoteNo++;
+  else
+    quoteNo = 0;
+}
+
+function processCurrentText() {
+  console.log('calling processCurrentText');
+  // get current input text and split it
+  let curr_input = input_area.value;
+  curr_input = input_area.value.substring(0, curr_input.length - 22);
+  curr_input_array = curr_input.split('');
+
+  // increment total characters typed
+  characterTyped++;
+
+  errors = 0;
+
+  quoteSpanArray = quote_text.querySelectorAll('span');
+  quoteSpanArray.forEach((char, index) => {
+    let typedChar = curr_input_array[index]
+
+    // characters not currently typed
+    if (typedChar == null) {
+      char.classList.remove('correct_char');
+      char.classList.remove('incorrect_char');
+
+      // correct characters
+    } else if (typedChar === char.innerText) {
+      char.classList.add('correct_char');
+      char.classList.remove('incorrect_char');
+
+      // incorrect characters
+    } else {
+      char.classList.add('incorrect_char');
+      char.classList.remove('correct_char');
+
+      // increment number of errors
+      errors++;
+    }
+  });
+
+  // display the number of errors
+  error_text.textContent = total_errors + errors;
+
+  // update accuracy text
+  let correctCharacters = (characterTyped - (total_errors + errors));
+  let accuracyVal = ((correctCharacters / characterTyped) * 100);
+  accuracy_text.textContent = Math.round(accuracyVal);
+
+  console.log(curr_input)
+  console.log(current_quote)
+
+  // if current text matches the quote
+  if (curr_input==current_quote) {
+    updateQuote();
+
+    // update total errors
+    total_errors += errors;
+
+    // clear the input area
+    input_area.value = "←(Your cursor is here)";
+  }
+}
+
+function updateTimer() {
+  if (timeLeft > 0) {
+    // decrease the current time left
+    timeLeft--;
+
+    // increase the time elapsed
+    timeElapsed++;
+
+    // update the timer text
+    timer_text.textContent = timeLeft + "s";
+  }
+  else {
+    // finish the game
+    finishGame();
+  }
+}
+
+function finishGame() {
+  // stop the timer
+  clearInterval(game_timer);
+
+  // disable the input area
+  input_area.disabled = true;
+
+  // show finishing text
+  quote_text.textContent = "Click on restart to start a new game.";
+
+  // display restart button
+  restart_btn.style.display = "block";
+
+  // calculate cpm and wpm
+  cpm = Math.round(((characterTyped / timeElapsed) * 60));
+  wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60));
+
+  // update cpm and wpm text
+  cpm_text.textContent = cpm;
+  wpm_text.textContent = wpm;
+
+  // display the cpm and wpm
+  cpm_group.style.display = "block";
+  wpm_group.style.display = "block";
+}
+
+function startGame() {
+  resetValues();
+  updateQuote();
+
+  // clear old and start a new timer
+  clearInterval(game_timer);
+  game_timer = setInterval(updateTimer, 1000);
+}
+
+function resetValues() {
+  timeLeft = TIME_LIMIT;
+  timeElapsed = 0;
+  errors = 0;
+  total_errors = 0;
+  accuracy = 0;
+  characterTyped = 0;
+  quoteNo = 0;
+  input_area.disabled = false;
+
+  input_area.value = "←(Your cursor is here)";
+  quote_text.textContent = 'Click on the area below to start the game.';
+  accuracy_text.textContent = 100;
+  timer_text.textContent = timeLeft + 's';
+  error_text.textContent = 0;
+  restart_btn.style.display = "none";
+  cpm_group.style.display = "none";
+  wpm_group.style.display = "none";
+}
 
 function error() {
   alert("Stream generation failed.");
@@ -238,18 +439,21 @@ function updatePitch(time) {
           origString.substring(0, origString.length - 23) +
           origString.substring(origString.length - 22);
         canvasContext.fillStyle = "#82e85b";
+        processCurrentText();
       } else if (pitch >= 130 && pitch < 180) {
         document.getElementById("maininput").value =
           origString.substring(0, origString.length - 22) +
           " " +
           origString.substring(origString.length - 22);
         canvasContext.fillStyle = "#ffd135";
+        processCurrentText();
       } else if (pitch >= 180) {
         document.getElementById("maininput").value =
           origString.substring(0, origString.length - 22) +
           randomVowel +
           origString.substring(origString.length - 22);
         canvasContext.fillStyle = "#ff351f";
+        processCurrentText();
       }
     }
 
