4c0ba14a5716c3de37eb48694752c9f6477fd34b
ktaekwon000 <ktaekwon000@gmail.com>
2021-01-09 00:23:56 +0800

    prototype frontend implementation

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,27 +1,17 @@
 var xmlhttp, xlmDoc;
 
 window.onload = function () {
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.open(
-    "GET",
-    "https://dreamlo.com/lb/5ff83c4c0af26924d032efa1/xml-asc",
-    false
-  );
-  xmlhttp.send();
-  xmlDoc = xmlhttp.responseXML;
-
-  document.write("<table border='1'>");
-  var x = xmlDoc.getElementsByTagName("CD");
-  for (i = 0; i < x.length; i++) {
-    document.write("<tr><td>");
-    document.write(
-      x[i].getElementsByTagName("entry")[0].childNodes[0].nodeValue
-    );
-    document.write("</td><td>");
-    document.write(
-      x[i].getElementsByTagName("score")[0].childNodes[0].nodeValue
-    );
+  fetch("https://taekwon.kim:3000/scores").then((response) => {
+    return response.json();
+  }).then((json) => {
+    document.write("<table border='1'>");
+    for (let item of json) {
+      document.write("<tr><td>");
+      document.write(item.name);
+      document.write("</td><td>");
+      document.write(item.score);
+      document.write("</td></tr>");
+    }
     document.write("</td></tr>");
-  }
-  document.write("</table>");
+  });
 };
